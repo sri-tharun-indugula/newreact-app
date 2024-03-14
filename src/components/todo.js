025016f1bd06ba571a79a9d9a4a67fba64bd0214
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-03-14 09:39:29 +0530

    testing new component

---
+++ src/components/todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Todo = () => (
+    <div>
+        <label>hello</label>
+        <input></input>
+    </div>
+);
+
+export default Todo;
